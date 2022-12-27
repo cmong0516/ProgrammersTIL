fc5f501b9417afca507b151a8fc1020fe6ce52cd
cmong0516 <cmong0516@gmail.com>
2022-12-27 13:25:47 +0900

    2022.12.27

---
+++ src/AntCorps.java
@@ -0,0 +1,21 @@
+public class AntCorps {
+    public static void main(String[] args) {
+        solution(24);
+    }
+
+    public static int solution(int hp) {
+        int answer = 0;
+
+        answer += hp/5;
+
+        hp = hp - 5*(hp/5);
+
+        answer += hp/3;
+
+        hp = hp - 3*(hp/3);
+
+        answer += hp/1;
+
+        return answer;
+    }
+}
