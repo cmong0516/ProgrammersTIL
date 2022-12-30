27d5f016d9f279d82f16d8a38ff2ae90a5726fb3
cmong0516 <cmong0516@gmail.com>
2022-12-31 00:51:11 +0900

    2022.12.31

--- src/MakeBiggistNumber.java
+++ src/MakeBiggistNumber.java
@@ -24,3 +24,4 @@ public class MakeBiggistNumber {
         return answer;
     }
 }
+
---
+++ src/RotationArray.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class RotationArray {
+    public static void main(String[] args) {
+        solution(new int[]{1, 2, 3}, "right");
+    }
+
+    public static int[] solution(int[] numbers, String direction) {
+
+        List<Integer> list = new ArrayList<>();
+
+        for (int number : numbers) {
+            list.add(number);
+        }
+
+        if (direction.equals("right")) {
+            list.remove(list.size()-1);
+            list.add(0,numbers[numbers.length-1]);
+        }
+
+        if (direction.equals("left")) {
+            list.remove(0);
+            list.add(numbers[0]);
+        }
+
+
+        return list.stream().mapToInt(i -> i).toArray();
+    }
+}
