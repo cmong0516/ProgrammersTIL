1997236ec93b342c6fd3ba2a4e6a50979a9a91b9
cmong0516 <cmong0516@gmail.com>
2022-12-31 00:33:56 +0900

    2022.12.31

---
+++ src/MakeBiggistNumber.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+public class MakeBiggistNumber {
+    public static void main(String[] args) {
+        solution(new int[]{1, 2, -3, 4, -5});
+    }
+
+    public static int solution(int[] numbers) {
+        Arrays.sort(numbers);
+        int answer = numbers[0] * numbers[1];
+
+        if (numbers[0] * numbers[1] > numbers[numbers.length - 1] * numbers[numbers.length - 2]) {
+            answer = numbers[numbers.length - 1] * numbers[numbers.length - 2];
+        }
+
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = 0; j < numbers.length; j++) {
+                if (numbers[i] != numbers[j] && numbers[i] * numbers[j] > answer) {
+                    answer = numbers[i] * numbers[j];
+                }
+            }
+        }
+
+        return answer;
+    }
+}
