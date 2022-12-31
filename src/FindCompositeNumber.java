4905ff411ff58b128af4e44d12610d79ab68a257
cmong0516 <cmong0516@gmail.com>
2022-12-31 23:47:07 +0900

    2022.12.31

---
+++ src/FindCompositeNumber.java
@@ -0,0 +1,25 @@
+public class FindCompositeNumber {
+    public static void main(String[] args) {
+        solution(10);
+    }
+
+    public static int solution(int n) {
+        int answer = 0;
+
+        for (int i = 1; i <= n; i++) {
+            int check = 0;
+            for (int j = 1; j <= i; j++) {
+                if (i % j == 0) {
+                    check++;
+
+                    if (check >= 3) {
+                        answer++;
+                        break;
+                    }
+                }
+            }
+        }
+
+        return answer;
+    }
+}
--- src/SortString2.java
+++ src/SortString2.java
@@ -21,3 +21,4 @@ public class SortString2 {
         return answer;
     }
 }
+
