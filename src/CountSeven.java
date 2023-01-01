eb4a079aa1154e40915e624930127f3726a87dfc
cmong0516 <cmong0516@gmail.com>
2023-01-01 17:46:15 +0900

    2023.01.01

---
+++ src/CountSeven.java
@@ -0,0 +1,20 @@
+public class CountSeven {
+    public static void main(String[] args) {
+        solution(new int[]{7, 77, 17});
+    }
+
+    public static int solution(int[] array) {
+        int answer = 0;
+
+        for (int i : array) {
+            String[] split = String.valueOf(i).split("");
+            for (String s : split) {
+                if (s.equals("7")) {
+                    answer++;
+                }
+            }
+        }
+
+        return answer;
+    }
+}
