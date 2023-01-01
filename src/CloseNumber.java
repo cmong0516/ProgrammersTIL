c19091ef3c21df5d6be227e728966c3b0c7bc9b1
cmong0516 <cmong0516@gmail.com>
2023-01-01 17:11:31 +0900

    2023.01.01

---
+++ src/CloseNumber.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class CloseNumber {
+    public static void main(String[] args) {
+        solution(new int[]{3,10,28}, 20);
+    }
+
+    public static int solution(int[] array, int n) {
+        int mostClose = 100;
+
+        Arrays.sort(array);
+
+        for (int i : array) {
+            if (Math.abs(n - i) < mostClose) {
+                mostClose = Math.abs(n - i);
+            }
+        }
+
+        List<Integer> list = new ArrayList<>();
+        for (int i : array) {
+            if (Math.abs(n - i) == mostClose) {
+                list.add(i);
+            }
+        }
+
+        return list.get(0);
+    }
+}
