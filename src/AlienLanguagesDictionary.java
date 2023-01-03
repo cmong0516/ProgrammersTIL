a7670ee52375f9662768118205774dfb3069ad5d
cmong0516 <cmong0516@gmail.com>
2023-01-03 19:17:53 +0900

    2023.01.03

---
+++ src/AlienLanguagesDictionary.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+public class AlienLanguagesDictionary {
+    public static void main(String[] args) {
+        solution(new String[]{"d", "z", "x"}, new String[]{"def", "dww", "dzx", "loveaw"});
+    }
+
+    public static int solution(String[] spell, String[] dic) {
+        int answer = 2;
+
+        Arrays.sort(spell);
+
+        for (String s : dic) {
+            String[] split = s.split("");
+            Arrays.sort(split);
+            if (Arrays.equals(spell, split)) {
+                answer = 1;
+            }
+        }
+
+
+
+        return answer;
+    }
+
+}
