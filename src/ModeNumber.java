35e3bf5553426cb963c30c1271ae2acec465950c
cmong0516 <cmong0516@gmail.com>
2023-01-07 19:31:46 +0900

    2023.01.07

--- src/ModeNumber.java
+++ src/ModeNumber.java
@@ -4,45 +4,35 @@ import java.util.List;
 
 public class ModeNumber {
     public static void main(String[] args) {
-        System.out.println(solution(new int[]{1,2,3,3,3,4}));
+        System.out.println(solution(new int[]{1,2,3,3,3,4,1,2,3,3,3,4,1,2,3,3,3,4,1,2,3,3,3,4,1,2,3,3,3,4}));
 
     }
     public static int solution(int[] array) {
         int answer = 0;
-        int temp = 0;
-        int temp1 = 0;
+        int max = 0;
 
-        if (array.length == 1) {
-            return array[0];
+
+        List<Integer> list = new ArrayList<>();
+
+        for (int i : array) {
+            list.add(i);
         }
 
-        List<Integer> cnt = new ArrayList<>(Collections.nCopies(1000,0));
-
-        for (int i = 1; i <= 1000; i++) {
-            for (int arr : array) {
-                if (i == arr) {
-                    cnt.set(i-1,cnt.get(i-1) + 1);
-                }
-            }
-        }
-
-        System.out.println("cnt = " + cnt);
+        System.out.println("list = " + list);
 
         for (int i = 0; i < 100; i++) {
-            int j = cnt.get(i);
-            if (temp == j) {
+            int frequency = Collections.frequency(list, i);
+            if (frequency == max) {
                 answer = -1;
             }
-
-            if (temp < j) {
-                temp = j;
-
+            if (frequency > max) {
+                max = frequency;
+                System.out.println("frequency = " + frequency);
+                System.out.println("max = " + max);
+                answer = i;
             }
 
-
         }
-
-
 
         return answer;
     }
