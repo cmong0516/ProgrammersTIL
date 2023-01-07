046957ef55f3a8ffb00195b13c160896dcd99a1c
cmong0516 <cmong0516@gmail.com>
2023-01-08 00:32:14 +0900

    2023.01.08

--- src/FountainAdd.java
+++ src/FountainAdd.java
@@ -1,22 +1,36 @@
+import java.util.Arrays;
+import java.util.List;
+
 public class FountainAdd {
     public static void main(String[] args) {
-        solution(9, 2, 1, 3);
+        solution(1, 2, 3, 4);
     }
     public static int[] solution(int denum1, int num1, int denum2, int num2) {
         int[] answer = new int[2];
 
-        if (num2 % num1 == 0) {
-            int a = num2 / num1;
+        int a = Math.max(num1, num2);
+        int b = Math.min(num1, num2);
 
-            answer[0] = (denum1 * a) + denum2;
-            answer[1] = num2;
-        }
+        int gcd = getGcd(a, b);
 
-        int lcm = num1 * num2 / getGcd(num1, num2);
+        int lcm = a * b / gcd;
 
-        if (a % b != 0) {
-            answer[0] = denum1 * (lcm / num1) + denum2 * (lcm / num2);
-            answer[1] = lcm;
+        answer[0] = denum1 * (lcm/num1) + denum2 * (lcm/num2);
+
+        answer[1] = lcm;
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        System.out.println("lcm = " + lcm);
+
+        System.out.println("Arrays.toString(answer) = " + Arrays.toString(answer));
+
+        int gcd1 = 0;
+
+        while (gcd1 != 1) {
+            gcd1 = getGcd(answer[0], answer[1]);
+            answer[0] /= gcd1;
+            answer[1] /= gcd1;
         }
 
         return answer;
