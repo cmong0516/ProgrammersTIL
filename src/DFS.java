aae6b7888a3b6fb7f216a2b28d6bc42df9cfc09e
cmong0516 <cmong0516@gmail.com>
2023-01-09 16:55:19 +0900

    2023.01.09

--- src/DFS.java
+++
@@ -1,9 +0,0 @@
-public class DFS {
-    public static void main(String[] args) {
-
-    }
-
-    public static void dfs() {
-
-    }
-}
--- src/SafeArea.java
+++ src/SafeArea.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class SafeArea {
     public static void main(String[] args) {
         int[][] a = {{0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, {0, 0, 1, 0, 0}, {0, 0, 0, 0, 0}};
@@ -7,37 +9,48 @@ public class SafeArea {
     public static int solution(int[][] board) {
         int answer = 0;
 
+        int[][] copyBoard = new int[board.length][board[0].length];
+
+        int tempIndex = 0;
+
+        for (int[] ints : board) {
+            copyBoard[tempIndex] = Arrays.copyOf(ints, board[tempIndex].length);
+            tempIndex++;
+        }
+
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[i].length; j++) {
-                // 00 01 02 03 04
-                // 10 11 12 13 14
-                // 20 21 22 23 24
-                // 30 31 32 33 34
-                // 40 41 42 43 44
+                int value = board[i][j];
 
-                if (board[i][j] != 1) {
-                    if (board[i][j - 1] != 1 && board[i][j + 1] != 1 && board[i + 1][j - 1] != 1
-                            && board[i + 1][j] != 1 && board[i + 1][j + 1] != 1) {
-                        answer++;
-                    }
-                    if (j == 0) {
-
-                    }
+                if (value == 0) {
+                    continue;
                 }
 
+                System.out.println("board[i][j] = " + board[i][j]);
             }
         }
-        System.out.println("answer = " + answer);
 
         return answer;
     }
-}
 
-//   왼위     && board[i][j - 1] != 1
-//   위     && board[i][j + 1] != 1
-//   오른위     && board[i + 1][j - 1] != 1
-//   왼     && board[i + 1][j] != 1
-//   오     && board[i + 1][j + 1] != 1
-//   왼아래     && board[i - 1][j - 1] != 1
-//   아래     && board[i - 1][j] != 1
-//   오른아래     && board[i - 1][j + 1] != 1
+    static void dfs(int[][] board, boolean[][] visited, int x, int y) {
+        int[] dx = {0, 0, -1, 1};
+        int[] dy = {-1, 1, 0, 0};
+        boolean[][] temp = visited.clone();
+
+        if (x == -1 || y == -1) {
+            return;
+        }
+        visited[x][y] = true;
+
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            if (!visited[nx][ny] && board[nx][ny] == 0) {
+                dfs(board, visited, nx, ny);
+                visited[nx][ny] = false;
+            }
+        }
+    }
+
+}
