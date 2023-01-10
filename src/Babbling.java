b3198a82e89cf934efa2576458dde2f8c8020673
cmong0516 <cmong0516@gmail.com>
2023-01-10 14:45:45 +0900

    2023.01.10

--- src/Babbling.java
+++ src/Babbling.java
@@ -1,7 +1,4 @@
 
-import java.util.HashSet;
-import java.util.Set;
-
 public class Babbling {
     public static void main(String[] args) {
         solution(new String[]{"ayaye", "uuuma", "ye", "yemawoo", "ayaa"});
@@ -10,28 +7,14 @@ public class Babbling {
         int answer = 0;
         String[] can = {"aya", "ye", "woo", "ma"};
 
-        Set<String> set = new HashSet<>();
-
-        for (int i = 0; i < can.length; i++) {
-            set.add(can[i]);
-            for (int j = i+1; j < can.length; j++) {
-                set.add(can[i] + can[j]);
-                for (int k = j + 1; k < can.length; k++) {
-                    set.add(can[i] + can[j] + can[k]);
-                }
-            }
-        }
-
-        System.out.println("set = " + set);
-
         for (String s : babbling) {
-            for (String s1 : set) {
-                if (s1.contains(s)) {
-                    answer++;
-                }
+            for (String c : can) {
+                s = s.replaceFirst(c, " ");
+            }
+            if (s.replaceAll(" ", "").equals("")) {
+                answer++;
             }
         }
-        System.out.println("answer = " + answer);
 
         return answer;
     }
