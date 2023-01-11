eb95d0e2631b8a521ef3eb7f8803cbe5983bad17
cmong0516 <cmong0516@gmail.com>
2023-01-11 13:32:56 +0900

    2023.01.11

--- src/MostCloseChar.java
+++ src/MostCloseChar.java
@@ -4,27 +4,29 @@ import java.util.List;
 
 public class MostCloseChar {
     public static void main(String[] args) {
-        solution("foobar");
+        solution("banana");
     }
 
     public static int[] solution(String s) {
         int[] answer = new int[s.length()];
         String[] split = s.split("");
         List<String> list = List.of(split);
-        answer[0] = -1;
 
-        for (int i = 1; i < s.length(); i++) {
+        for (int i = 1; i <= s.length(); i++) {
             String substring = s.substring(0,i-1);
             System.out.println("substring = " + substring);
-            if (!substring.contains(split[i])) {
-                answer[i] = -1;
+            System.out.println("split[i-1] = " + split[i-1]);
+
+            if (substring.lastIndexOf(split[i - 1]) == -1) {
+                answer[i-1] =  substring.lastIndexOf(split[i - 1]);
             }
-            if (substring.contains(split[i])) {
+
+            if (substring.lastIndexOf(split[i - 1]) != -1) {
                 System.out.println("i = " + i);
-                System.out.println("split[i] = " + split[i]);
-                System.out.println("substring.lastIndexOf(split[i]) = " + substring.lastIndexOf(split[i]));
-                answer[i] = i - substring.lastIndexOf(split[i]);
+                System.out.println("substring.lastIndexOf(split[i - 1]) = " + substring.lastIndexOf(split[i - 1]));
+                answer[i-1] = i-1-substring.lastIndexOf(split[i - 1]);
             }
+
         }
 
         System.out.println(Arrays.toString(answer));
