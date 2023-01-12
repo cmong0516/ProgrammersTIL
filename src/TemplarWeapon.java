9359fa1ecdcb9cd061412d5282139dcc2a0532e7
cmong0516 <cmong0516@gmail.com>
2023-01-12 18:00:01 +0900

    2023.01.12

--- src/TemplarWeapon.java
+++ src/TemplarWeapon.java
@@ -12,12 +12,7 @@ public class TemplarWeapon {
         List<Integer> divisors = new ArrayList<>();
 
         for (int i = 1; i <= number; i++) {
-            int cnt = 0;
-            for (int j = 1; j <= i; j++) {
-                if (i % j == 0) {
-                    cnt++;
-                }
-            }
+            int cnt = countDivisors(i);
             if (cnt > limit) {
                 answer+=power;
             }
@@ -26,8 +21,6 @@ public class TemplarWeapon {
             }
 
         }
-
-        System.out.println(divisors);
 
         return answer;
     }
