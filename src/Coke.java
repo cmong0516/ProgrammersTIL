c7a9dfcf7430ea8cd9d459a9d16492880d62a4ad
cmong0516 <cmong0516@gmail.com>
2023-01-14 22:48:52 +0900

    2023.01.14

--- src/Coke.java
+++ src/Coke.java
@@ -1,19 +1,24 @@
 public class Coke {
     public static void main(String[] args) {
-        solution(3, 2, 20);
+        solution(2, 1, 1000000);
     }
 
     public static int solution(int a, int b, int n) {
         int answer = 0;
 
         while (n >= a) {
-            int coke = (n / a) * b;
+            long coke = (n / a) * b;
+            System.out.println("얻은 콜라 = " + coke);
             n -= (coke * a) / b;
+            System.out.println("남은 병수 = " + n);
             answer += coke;
             n += coke;
+            System.out.println("콜라 다마시고 총 병수 = " + n);
             coke = 0;
 
         }
+
+
 
         return answer;
     }
