762f1154efd1f54efd7f6f658340ed680530215b
cmong0516 <cmong0516@gmail.com>
2023-01-23 20:45:51 +0900

    2023.01.23

--- src/TileN2.java
+++ src/TileN2.java
@@ -5,6 +5,17 @@ public class TileN2 {
 
     public static int solution(int n) {
         int answer = 0;
+
+        int[] arr = new int[n];
+        // 1 , 2     1x + 2y = n;
+        // 1 2 3 5 8
+        for (int i = 2; i < n; i++) {
+            int num = arr[i - 1] + arr[i - 2];
+            arr[i] = num % 1000000007;
+        }
+
+        answer = arr[n - 1];
+
         return answer;
     }
 }
