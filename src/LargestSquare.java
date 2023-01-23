75517e1e67f9c32a99d7aa0aa00178aebc911751
cmong0516 <cmong0516@gmail.com>
2023-01-24 05:38:55 +0900

    2023.01.24

---
+++ src/LargestSquare.java
@@ -0,0 +1,12 @@
+public class LargestSquare {
+    public static void main(String[] args) {
+        int[][] a = {{0, 1, 1, 1}, {1, 1, 1, 1}, {1, 1, 1, 1}, {0, 0, 1, 0}};
+        solution(a);
+    }
+
+    public static int solution(int[][] board) {
+        int answer = 1234;
+
+        return answer;
+    }
+}
