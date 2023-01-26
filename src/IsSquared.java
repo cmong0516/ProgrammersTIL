b388f793f34f2a29e412128a6c4ea4f99b975393
cmong0516 <cmong0516@gmail.com>
2023-01-27 04:39:45 +0900

    2023.01.27

--- src/IsSquared.java
+++ src/IsSquared.java
@@ -15,5 +15,3 @@ public class IsSquared {
         return answer;
     }
 }
-
-
---
+++ src/WayToStandInLine.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class WayToStandInLine {
+    public static void main(String[] args) {
+        solution(3, 5);
+    }
+
+    public static int[] solution(int n, long k) {
+
+
+        int[] answer = new int[n];
+
+        List<Integer> people = new ArrayList<>();
+        long factorial = 1;
+        int index = 0;
+
+        for(int i=1; i<=n; i++) {
+            factorial *= i;
+            people.add(i);
+        }
+
+        k--;
+        while(n > 0) {
+            factorial /= n;
+            int value = (int) (k / factorial);
+            answer[index++] = people.get(value);
+            people.remove(value);
+
+            k %= factorial;
+            n--;
+        }
+
+        return answer;
+    }
+}
