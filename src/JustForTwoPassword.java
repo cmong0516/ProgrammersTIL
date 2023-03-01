fffea780331d05b925e7216abf3bf058ffd87f4a
cmong0516 <cmong0516@gmail.com>
2023-03-01 18:28:04 +0900

    2023.03.01

---
+++ src/JustForTwoPassword.java
@@ -0,0 +1,12 @@
+public class JustForTwoPassword {
+    public static void main(String[] args) {
+        solution("aukks","wbqd",5);
+    }
+
+    static String solution(String s,String skip,int index) {
+        char[] arr = new char[26];
+
+
+        return null;
+    }
+}
--- src/NQueen.java
+++ src/NQueen.java
@@ -40,3 +40,4 @@ public class NQueen {
         return true;
     }
 }
+
