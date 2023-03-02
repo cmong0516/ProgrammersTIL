69c7e7ca725d991dd15943ec4e2eaca7c6537aa6
cmong0516 <cmong0516@gmail.com>
2023-03-03 04:55:53 +0900

    2023.03.03

--- src/JustForTwoPassword.java
+++ src/JustForTwoPassword.java
@@ -1,16 +1,50 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class JustForTwoPassword {
     public static void main(String[] args) {
         solution("aukks","wbqd",5);
     }
 
     static String solution(String s,String skip,int index) {
+        String answer = "";
+
         char[] arr = new char[26];
+
+        char[] chars = s.toCharArray();
+
+        char[] skipChars = skip.toCharArray();
+
+        List<Character> charList = new ArrayList<>();
+
+        for (char skipChar : skipChars) {
+            charList.add(skipChar);
+        }
 
         arr[0] = 'a';
 
-        for (int i = 1; i < 25; i++) {
+        for (int i = 1; i < 26; i++) {
             arr[i] = (char) (arr[i-1]+1);
         }
-        return null;
+
+        for (char c : chars) {
+            for (int i = 0; i < index; i++) {
+                c = (char)(c +1);
+                if (c == 'z' + 1) {
+                    c = 'a';
+                }
+                if (charList.contains(c)) {
+                    c = (char)(c+1);
+                }
+                if (c == 'z' + 1) {
+                    c = 'a';
+                }
+            }
+            answer += String.valueOf(c);
+
+        }
+
+        return answer;
+
     }
 }
