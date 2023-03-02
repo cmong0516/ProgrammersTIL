8a6c7d78c47e12d7c4caf2609b72ab6f0046b976
cmong0516 <cmong0516@gmail.com>
2023-03-02 21:52:53 +0900

    2023.03.02

--- src/JustForTwoPassword.java
+++ src/JustForTwoPassword.java
@@ -6,7 +6,11 @@ public class JustForTwoPassword {
     static String solution(String s,String skip,int index) {
         char[] arr = new char[26];
 
+        arr[0] = 'a';
 
+        for (int i = 1; i < 25; i++) {
+            arr[i] = (char) (arr[i-1]+1);
+        }
         return null;
     }
 }
