5bf813cea2e62ed3d9c6ab47edd64f2b24ce9b8c
cmong0516 <cmong0516@gmail.com>
2023-04-28 03:04:52 +0900

    2023.04.28

---
+++ src/HIndex.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+
+public class HIndex {
+    public static void main(String[] args) {
+        solution(new int[]{3, 0, 6, 1, 5});
+    }
+
+    public static int solution(int[] citations) {
+        int answer = 0;
+
+        Arrays.sort(citations);
+
+        for (int i = 0; i < citations.length; i++) {
+            int h = citations.length - i;
+
+            if (citations[i] >= h) {
+                answer = h;
+                break;
+            }
+        }
+
+        return answer;
+    }
+
+}
--- src/RotateBrackets.java
+++ src/RotateBrackets.java
@@ -1,10 +1,37 @@
+import java.util.Stack;
+
 public class RotateBrackets {
     public static void main(String[] args) {
         solution("[](){}");
     }
 
     public static int solution(String s) {
-        int answer = -1;
+        int answer = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+
+            Stack<Character> stack = new Stack<>();
+            String str = s.substring(i, s.length()) + s.substring(0, i);
+
+            for (int j = 0; j < str.length(); j++) {
+                if (stack.isEmpty()) {
+                    stack.push(str.charAt(j));
+                } else if (str.charAt(i) == ')' && stack.peek() == '(') {
+                    stack.pop();
+                } else if (str.charAt(i) == ']' && stack.peek() == '[') {
+                    stack.pop();
+                } else if (str.charAt(i) == '}' && stack.peek() == '{') {
+                    stack.pop();
+                } else {
+                    stack.push(str.charAt(j));
+                }
+            }
+
+            if (stack.isEmpty()) {
+                answer++;
+            }
+        }
+
         return answer;
     }
 }
