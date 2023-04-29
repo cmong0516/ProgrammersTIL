aaaa1e277898b750e3b78007de8e08a94199e4a5
cmong0516 <cmong0516@gmail.com>
2023-04-30 00:29:55 +0900

    2023.04.30

---
+++ src/ContinuousNumberOfSums.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class ContinuousNumberOfSums {
+    public static void main(String[] args) {
+        solution(new int[]{7, 9, 1, 1, 4});
+    }
+
+    public static int solution(int[] elements) {
+        Set<Integer> set = new HashSet<>();
+
+        int[] elements2 = new int[elements.length * 2];
+
+        for (int i = 0; i < elements.length; i++) {
+
+            elements2[i] = elements2[i+elements.length] =elements[i];
+        }
+
+        for (int i = 1; i <= elements.length; i++) {
+            for (int j = 0; j < elements.length*2; j++) {
+                set.add(Arrays.stream(Arrays.copyOfRange(elements2, j, j + i)).sum());
+            }
+        }
+
+        return set.size();
+    }
+}
