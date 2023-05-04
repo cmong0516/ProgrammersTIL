931ff215532d6feae790cec2efa1dbca327380ae
cmong0516 <cmong0516@gmail.com>
2023-05-05 04:51:43 +0900

    2023.05.05

--- src/ParkingFeeCalculation.java
+++ src/ParkingFeeCalculation.java
@@ -1,19 +1,19 @@
 import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
 public class ParkingFeeCalculation {
     public static void main(String[] args) {
         solution(new int[]{180, 5000, 10, 600},
-                new String[]{"05:34 5961 IN", "06:00 0000 IN", "06:34 0000 OUT", "07:59 5961 OUT", "07:59 0148 IN",
-                        "18:59 0000 IN", "19:09 0148 OUT", "22:59 5961 IN", "23:00 5961 OUT"});
+                new String[]{"05:34 5961 IN", "06:34 5961 OUT", "07:34 5961 IN", "08:34 5961 OUT", "09:34 5961 IN", "10:34 5961 OUT", "11:34 5961 IN", "12:34 5961 OUT"});
     }
 
-    public static int[] solution(int[] fees, String[] records) {
-        int[] answer = {};
+    public static int[] solution(int[] fees, String[] records){
 
-        Map<String, String> map = new HashMap<>();
-        Map<String, Integer> pay = new HashMap<>();
+        Map<String, String> inTimeMap = new HashMap<>();
+        Map<String, Integer> takeTimeMap = new HashMap<>();
 
         for (String record : records) {
             String time = record.split(" ")[0];
@@ -21,23 +21,83 @@ public class ParkingFeeCalculation {
             String inAndOut = record.split(" ")[2];
 
             if (inAndOut.equals("IN")) {
-                map.put(num,time);
+                inTimeMap.put(num,time);
             }
 
             if (inAndOut.equals("OUT")) {
-                String inTime = map.get(num);
+                String inTime = inTimeMap.get(num);
                 System.out.println("num = " + num);
                 System.out.println("inTime = " + inTime);
                 System.out.println("outTime = " + time);
-                map.remove(num);
+                SimpleDateFormat simpleDateFormat = new SimpleDateFormat("HH:mm");
+
+                try {
+                    Date inTimeDate = simpleDateFormat.parse(inTime);
+                    Date outTime = simpleDateFormat.parse(time);
+
+                    long l = (outTime.getTime() - inTimeDate.getTime())/1000/60;
+                    System.out.println("l = " + l);
+
+                    takeTimeMap.put(num,takeTimeMap.getOrDefault(num,0) + (int)l);
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+                inTimeMap.remove(num);
             }
         }
 
-        System.out.println("map = " + map);
+        for (String num : inTimeMap.keySet()) {
+            SimpleDateFormat simpleDateFormat = new SimpleDateFormat("HH:mm");
+            String closeTime = "23:59";
+
+            String inTime = inTimeMap.get(num);
+
+            try {
+                Date inTimeDate = simpleDateFormat.parse(inTime);
+                Date closeTimeDate = simpleDateFormat.parse(closeTime);
+
+                long l = (closeTimeDate.getTime() - inTimeDate.getTime()) / 1000 / 60;
+
+                takeTimeMap.put(num, (int) (takeTimeMap.getOrDefault(num,0) + l));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        System.out.println("inTimeMap = " + inTimeMap);
+        System.out.println("takeTimeMap = " + takeTimeMap);
+
+        return pay(takeTimeMap,fees);
+    }
+
+    public static int[] pay(Map<String, Integer> takeTimeMap,int[]fees) {
+
+
+        int[] answer = new int[takeTimeMap.size()];
+        Object[] objects = takeTimeMap.keySet().toArray();
+        Arrays.sort(objects);
+
+        int i = 0;
+
+        for (Object object : objects) {
+            Integer takeTime = takeTimeMap.get(object);
+            System.out.println("takeTime = " + takeTime);
+            System.out.println("fees[0] = " + fees[0]);
+
+            if (takeTime > fees[0]) {
+                answer[i] = fees[1] + (int) Math.ceil((takeTime - fees[0]) / (double)fees[2]) * fees[3];
+            } else {
+                answer[i] = fees[1];
+            }
+
+            i++;
+        }
+
+        System.out.println("answer = " + Arrays.toString(answer));
 
         return answer;
     }
 }
-
-
-//    SimpleDateFormat simpleDateFormat = new SimpleDateFormat("HH:mm");
+// 1 3 4 7 8 10
