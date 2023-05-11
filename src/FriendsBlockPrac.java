9e2376db7f4f61b3866f71318a78b85c9de0a08b
cmong0516 <cmong0516@gmail.com>
2023-05-12 04:12:35 +0900

    2023.05.12

--- src/FriendsBlockPrac.java
+++ src/FriendsBlockPrac.java
@@ -1,59 +1,34 @@
+import java.awt.image.ImageProducer;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class Friends4Block {
+public class FriendsBlockPrac {
 
-    static char[][] map;
     static int answer = 0;
+    static char[][] map;
 
     public static void main(String[] args) {
         solution(4, 5, new String[]{"CCBDE", "AAADE", "AAABF", "CCBBF"});
     }
 
-    public static int solution(int m, int n, String[] board) {
-
-
-         map = makeCharMap(m, n, board);
-
-        for (char[] chars : map) {
-            System.out.println("chars = " + Arrays.toString(chars));
-        }
-        System.out.println();
-
-        while (updateBlocks(m, n)) {
-            for (char[] chars : map) {
-                System.out.println("chars = " + Arrays.toString(chars));
-            }
-            System.out.println();
-        }
-
-        System.out.println("answer = " + answer);
-
-        return answer;
-    }
-
-    private static boolean updateBlocks(int m, int n) {
+    public static boolean updateBlocks(int m, int n) {
         boolean[][] visit = new boolean[m][n];
         int cnt = 0;
 
-        isVisit(m, n, visit);
+        isVisit(m,n,visit);
 
         for (int i = 0; i < n; i++) {
             List<Character> temp = new ArrayList<>();
-            System.out.println("i = " + i);
-            System.out.println();
+
             for (int j = m-1; j >= 0; j--) {
-                System.out.println("j = " + j);
-                System.out.println();
                 if (visit[j][i]) {
                     cnt++;
                     continue;
                 }
+
                 temp.add(map[j][i]);
             }
-
-            System.out.println("temp = " + temp);
 
             for (int j = m-1,k=0; j >= 0 ; j--,k++) {
                 if (k < temp.size()) {
@@ -69,36 +44,47 @@ public class Friends4Block {
         return cnt > 0;
     }
 
-    private static void isVisit(int m, int n, boolean[][] visit) {
+    public static void isVisit(int m, int n, boolean[][] visit) {
         for (int i = 0; i < m - 1; i++) {
             for (int j = 0; j < n - 1; j++) {
-
                 if (map[i][j] == '0') {
                     continue;
                 }
+
                 if (check(i, j)) {
                     visit[i][j] = true;
-                    visit[i][j+1] = true;
                     visit[i+1][j] = true;
+                    visit[i][j+1] = true;
                     visit[i+1][j+1] = true;
                 }
             }
         }
     }
 
-    public static char[][] makeCharMap(int m, int n, String[] board) {
-        char[][] map = new char[m][n];
+    public static int solution(int m, int n, String[] board) {
 
-        for (int i = 0; i < m; i++) {
-            map[i] = board[i].toCharArray();
+        char[][] map = makeBoard(m, n, board);
+
+        while (updateBlocks(m,n)) {
         }
 
-        return map;
+        return answer;
     }
 
     public static boolean check(int i, int j) {
         char c = map[i][j];
 
         return map[i + 1][j] == c && map[i][j + 1] == c && map[i + 1][j + 1] == c;
+    }
+
+    public static char[][] makeBoard(int m, int n, String[] board) {
+
+        map = new char[m][n];
+
+        for (int i = 0; i < m; i++) {
+            map[i] = board[i].toCharArray();
+        }
+
+        return map;
     }
 }
