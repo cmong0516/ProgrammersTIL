0f60740300e662b4db7475bc563c2d9ca16524ef
cmong0516 <cmong0516@gmail.com>
2023-07-21 09:45:31 +0900

    2023.07.21

---
+++ src/SplitSpace.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class SplitSpace {
+    public static void main(String[] args) {
+        solution("programmers"	);
+    }
+
+    public static String[] solution(String my_string) {
+        String[] split = my_string.split(" ");
+
+        String[] answer = split;
+
+        return answer;
+    }
+}
