2478eacdc824d25fab684d230d40a9b2e74aba46
cmong0516 <cmong0516@gmail.com>
2023-09-07 22:05:14 +0900

    2023.09.07

---
+++ src/DiskController.java
@@ -0,0 +1,14 @@
+public class DiskController {
+    public static void main(String[] args) {
+        int[][] jobs = {{0, 3}, {1, 9}, {2, 6}};
+        solution(jobs);
+    }
+
+    public static int solution(int[][] jobs) {
+        // 요청시간 , 소요시간 에 대한 배열이 주어진다.
+        // 먼저 처리하던 작업을 완료하고 다음 작업을 수행하는데
+        // 완료시간 - 요청시간 의 총합 / 작업의수 를 최소로 만들어라.
+
+
+    }
+}
