d847d91ff54a823707281a1c06ab058f1d5a1cbf
cmong0516 <cmong0516@gmail.com>
2023-09-09 23:54:23 +0900

    2023.09.09

---
+++ src/WorkPark.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+
+public class WorkPark {
+    static char[][] map;
+
+    public static void main(String[] args) {
+        solution(new String[]{"SOO", "OOO", "OOO"}, new String[]{"E 2", "S 2", "W 1"});
+    }
+
+    public static int[] solution(String[] park, String[] routes) {
+        map = new char[park.length][park[0].length()];
+
+        for (int i = 0; i < park.length; i++) {
+            for (int j = 0; j < park[0].length(); j++) {
+                map[i][j] = park[i].charAt(j);
+            }
+        }
+
+        for (char[] chars : map) {
+            System.out.println("chars = " + Arrays.toString(chars));
+        }
+
+        return null;
+    }
+
+    public int[] move(String[] routes) {
+
+
+
+        return null;
+    }
+}
