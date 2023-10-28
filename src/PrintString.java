ab903fd4f0027d4df03294542f8feff33c23308a
cmong0516 <cmong0516@gmail.com>
2023-10-28 11:25:15 +0900

    2023.10.28

---
+++ src/PrintString.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class PrintString {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+
+        System.out.println(a);
+    }
+}
