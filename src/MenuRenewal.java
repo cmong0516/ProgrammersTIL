7e6654b4001ec18818ff5c9d05e3c0887d38d2e6
cmong0516 <cmong0516@gmail.com>
2023-11-05 09:18:29 +0900

    2023.11.05

---
+++ src/MenuRenewal.java
@@ -0,0 +1,10 @@
+public class MenuRenewal {
+    public static void main(String[] args) {
+        solution(new String[]{"ABCFG", "AC", "CDE", "ACDE", "BCFG", "ACDEH"}, new int[]{2, 3, 4});
+    }
+
+    public static String[] solution(String[] orders, int[] course) {
+        String[] answer = {};
+        return answer;
+    }
+}
